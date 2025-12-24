d7a065515cb6777be7df077188f301799e7ee6c8
MmdNazim <miamdnazim@gmail.com>
2025-12-24 22:25:55 +0600

    learing polymorphism in python

---
+++ polymorphism.py
@@ -0,0 +1,15 @@
+"""
+Docstring for polymorphism:
+---------------------------
+1.What is Polymorphism in Python?
+--> The literal meaning of Polymorphism is - having different forms. 
+In programming, Polymorphism refers to a function having the same name but being used in different ways and different scenarios.
+This makes programming easier and more intuitive.
+
+--> The word "polymorphism" means "many forms", and in programming it refers to methods/functions/operators with the same name that can be executed on many objects or classes.
+
+Here is the links that's helps us to understand the details:
+------------------------------------------------------------
+1.https://www.scaler.com/topics/python/polymorphism-in-python/
+2.https://www.geeksforgeeks.org/python/polymorphism-in-python/
+"""
